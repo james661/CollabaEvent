60ca72dc5bef9819462690d27b53c3f78fde962d
Daniel Munoz <daniel.munoz.me1@gmail.com>
2023-07-12 10:19:55 -0700

    working on server.js

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,2 +1,13 @@
 const router = require("express").Router();
 
+router.post('/login', (req, res) => {
+  // saving the user's username/password as a const
+  // by using parameters of req
+  const { username, password } = req.body;
+  
+  
+  // send succesful login response back to client
+  res.status(200).json({ message: "Login successful" });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(
 );
 
 const hbs = exphbs.create({});
+const loginRoutes = 
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -42,6 +43,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use(require("./controllers"));
+
+app.use(loginRoutes)
 
 sequelize
   .sync({ force: false })
